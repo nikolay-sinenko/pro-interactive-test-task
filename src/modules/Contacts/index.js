0cbb5afae06c2d03b5867eef2e7492cf8658c7de
nikolay-sinenko <n.sinenko47@gmail.com>
2021-10-14 04:34:11 +0300

    feat: add icons to contacts module

--- src/modules/Contacts/index.js
+++ src/modules/Contacts/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import Icon from 'ui/Icon';
+
 import style from './style.scss';
 import classNames from 'classnames/bind';
 const cx = classNames.bind(style);
@@ -9,15 +11,18 @@ const cx = classNames.bind(style);
  */
 const contactInfo = [
     {
+        name: 'phone',
         heading: 'Телефон',
         caption: '+7 495 123 45 67',
     },
     {
+        name: 'address',
         heading: 'Адрес',
         caption:
             '109382, Москва, пр. Егорьевский, д.2а, стр.10 въезд на машине только с улицы Люблинская',
     },
     {
+        name: 'email',
         heading: 'Почта',
         caption: '[Info@test.ru](mailto:info@test.ru)',
     },
@@ -30,6 +35,7 @@ const Contacts = () => (
     <>
         {contactInfo.map(item => (
             <div className={cx('item')} key={`contact-${item.name}`}>
+                <Icon name={item.name} />
                 <h3 className={cx('heading')}>{item.heading}</h3>
                 <div className={cx('caption')}>
                     <ReactMarkdown>{item.caption}</ReactMarkdown>

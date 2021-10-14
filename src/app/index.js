5f0ec1b534641ec32c61fca59df23800f5831620
nikolay-sinenko <n.sinenko47@gmail.com>
2021-10-14 04:03:33 +0300

    feat: add app root component

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './style.scss';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(style);
+
+/**
+ *  Application
+ */
+const App = () => (
+        <header className={cx('header')}>
+        </header>
+);
+
+export default App;

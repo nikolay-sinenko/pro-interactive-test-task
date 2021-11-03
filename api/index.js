a4d56d00d2179ffafbb9069b18837ba5ea3a3124
nikolay-sinenko <n.sinenko47@gmail.com>
2021-11-03 07:00:59 +0300

    refactor(api): migrate api to serverless format
    Refactor api's index from classic Express app to serverless

--- api/index.js
+++ api/index.js
@@ -1,32 +1,12 @@
-const { resolve } = require('path');
-
-const express = require('express');
+const app = require('express')();
 const cors = require('cors');
 
-const PORT = process.env.PORT || 5000;
-
-const app = express();
-
 //  Setup CORS preferences
-app.use(
-    cors({
-        origin: '*',
-    })
-);
-
-//  Serve files from production bundle
-app.use(express.static(resolve(__dirname, '../dist')));
-
-app.get('/', (_, res) => {
-    res.redirect('/index.html');
-});
+app.use(cors({ origin: '*' }));
 
 //  Apply routes
-app.use('/news', require('./routes/news'));
-app.use('/products', require('./routes/products'));
-app.use('/orders', require('./routes/orders'));
+app.use('/api/news', require('./routes/news'));
+app.use('/api/products', require('./routes/products'));
+app.use('/api/orders', require('./routes/orders'));
 
-//  Start API-server
-app.listen(PORT, () => {
-    console.log(`Server started at ${PORT}`);
-});
+module.exports = app;
